f38da2f2213d419d5498fe01341bb0cb1728a747
JiWangreal <jiwangreal@163.com>
2023-04-11 23:19:22 +0800

    XML P2

--- yazi-xml/main.cpp
+++ yazi-xml/main.cpp
@@ -18,10 +18,11 @@ void performanceTest()
     int start_sec = tv.tv_sec;
     int start_usec = tv.tv_usec;
     const int max = 10000;
-    for (int i = 0;i < max;++0)
+    for (int i = 0;i < max;++i)
     {
         Xml root;
         root.parse(str);
+        std::cout << root.str() << std::endl;
         root.clear();
     }
 
@@ -43,11 +44,21 @@ int main()
     Xml& student = root["student"];
 
     int id = student.attr("id");
+    //  error: cannot bind non-const lvalue reference of type ‘std::string&’ {aka ‘std::__cxx11::basic_string<char>&’} to an rvalue of type ‘std::string’ {aka ‘std::__cxx11::basic_string<char>’}
+/**
+因为强转得到一个临时对象string，是个右值
+In file included from main.cpp:3:
+./xml/Xml.h:35:5: note:   after user-defined conversion: ‘yazi::xml::Value::operator std::string()’
+   35 |     operator string();
+      |     ^~~~~~~~
+**/
+    std::string& out_class = student.attr("class");//error
     const string& name = student["name"].text();
     const string& age = student["age"].text();
     const string& gender = student["gender"].text();
 
     std::cout << id << "," << age << "," << gender << std::endl;
+    std::cout << out_class << std::endl;
 
     root.clear();
     return 0;
